ed74560faab2493ed013aa64909b5c96d643e756
Claus Thomasen <clt@miracle.dk>
2020-03-11 19:07:34 +0100

    Added bounce sound etc.

--- OculusQuestTest/Assets/Scripts/BallController.cs
+++ OculusQuestTest/Assets/Scripts/BallController.cs
@@ -1,35 +1,17 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR;
+﻿using UnityEngine;
 
 public class BallController : MonoBehaviour {
-    public Transform player;
-    public int speed = 2;
+    public AudioSource source;
 
     void Start() {
-
-        Debug.Log($"XR device is present: {XRDevice.isPresent}");
-        
-        var inputDevices = new List<InputDevice>();
-        InputDevices.GetDevices(inputDevices);
-
-        if (inputDevices.Count == 0) {
-            Debug.Log("No input device found!");
-        }
-        else {
-            foreach (var device in inputDevices) {
-                Debug.Log($"Device found with name '{device.name}' and role '{device.characteristics.ToString()}'");
-            }
-        }
+        source = GetComponent<AudioSource>();
     }
 
     void Update() {
-        //var moveHorizontally = Input.GetAxis("Horizontally");
-        // Debug.Log($"Horizontally {moveHorizontally}");
-        // var moveVertically = Input.GetAxis("Vertically");
-        var current = player.position;
-        // current.x += moveHorizontally * speed * Time.deltaTime;
-        // current.z += moveVertically * speed * Time.deltaTime;
-        player.position = current;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("Boing!");
+        source.Play();
     }
 }
--- OculusQuestTest/Assets/Scripts/GameController.cs
+++ OculusQuestTest/Assets/Scripts/GameController.cs
@@ -1,17 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class GameController : MonoBehaviour
-{
-    void Start()
-    {
+public class GameController : MonoBehaviour {
+    void Start() {
         Debug.Log($"Game started with XR {XRSettings.enabled}");
-        
+        Debug.Log($"XR device is present: {XRDevice.isPresent}");
+
+        var inputDevices = new List<InputDevice>();
+        InputDevices.GetDevices(inputDevices);
+
+        if (inputDevices.Count == 0) {
+            Debug.Log("No input device found!");
+        }
+        else {
+            foreach (var device in inputDevices) {
+                Debug.Log($"Device found with name '{device.name}' and role '{device.characteristics.ToString()}'");
+            }
+        }
     }
 
-    void Update()
-    {
+    void Update() {
     }
 }
