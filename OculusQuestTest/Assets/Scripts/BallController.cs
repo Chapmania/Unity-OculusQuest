d08017dca25f603e9cb3798f5123ff89f32ac2e7
Claus Thomasen <clt@miracle.dk>
2020-03-11 17:30:43 +0100

    First

---
+++ OculusQuestTest/Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class BallController : MonoBehaviour {
+    public Transform player;
+    public int speed = 2;
+
+    void Start() {
+
+        Debug.Log($"XR device is present: {XRDevice.isPresent}");
+        
+        var inputDevices = new List<InputDevice>();
+        InputDevices.GetDevices(inputDevices);
+
+        if (inputDevices.Count == 0) {
+            Debug.Log("No input device found!");
+        }
+        else {
+            foreach (var device in inputDevices) {
+                Debug.Log($"Device found with name '{device.name}' and role '{device.characteristics.ToString()}'");
+            }
+        }
+    }
+
+    void Update() {
+        //var moveHorizontally = Input.GetAxis("Horizontally");
+        // Debug.Log($"Horizontally {moveHorizontally}");
+        // var moveVertically = Input.GetAxis("Vertically");
+        var current = player.position;
+        // current.x += moveHorizontally * speed * Time.deltaTime;
+        // current.z += moveVertically * speed * Time.deltaTime;
+        player.position = current;
+    }
+}
---
+++ OculusQuestTest/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log($"Game started with XR {XRSettings.enabled}");
+        
+    }
+
+    void Update()
+    {
+    }
+}
