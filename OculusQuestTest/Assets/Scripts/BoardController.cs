02e7c447999132b110d0e0c759dcc9248f6035a0
Claus Thomasen <clt@miracle.dk>
2020-03-12 16:19:40 +0100

    UI stuff

---
+++ OculusQuestTest/Assets/Scripts/BoardController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardController : MonoBehaviour {
+    public Button button;
+
+    void Start() {
+        button.onClick.AddListener(() => { Debug.Log("Start!"); });
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
