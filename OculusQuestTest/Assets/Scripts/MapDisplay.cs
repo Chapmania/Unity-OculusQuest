99a43a9992dcd92b44f89c6d5229787a87efb065
Claus Thomasen <clt@miracle.dk>
2020-03-14 15:36:05 +0100

    Map size

--- OculusQuestTest/Assets/Scripts/MapDisplay.cs
+++ OculusQuestTest/Assets/Scripts/MapDisplay.cs
@@ -5,6 +5,6 @@ public class MapDisplay : MonoBehaviour {
 
     public void DrawTexture(Texture texture) {
         textureRenderer.sharedMaterial.mainTexture = texture;
-        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+        // textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
     }
 }
